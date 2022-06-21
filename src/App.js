3a837407cc4d27d06e1d38f0b69152bd319ee083
Harshil Bhavsar <harshilb150799@gmail.com>
2022-06-21 16:21:24 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Layout from "./components/Layout";
+import Education from "./pages/Education";
+import Experience from "./pages/Experience";
+import Home from "./pages/Home";
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,78 @@
+import React from "react";
+import avtar from "./../images/logo.png";
+
+function Home() {
+    return (
+        <div className="min-h-screen">
+            <div className="container pt-5 d-flex flex-column align-items-center">
+                <img src={avtar} width={200} alt="Pratham Bhavsar" />
+                <h1>Pratham Bhavsar</h1>
+                <h4>(Software Engineer)</h4>
+                <p className="text-center border-bottom pb-3">An aspiring computer engineer with skills in developing amazing solutions for real-time problems and possesing love for the web development. </p>
+                <p className="text-center">
+                    <span className="d-mobile-block">Email: bhavsarpratham2112@gmail.com</span>
+                    <span className="d-none d-web-contents"> | </span>
+                    <span className="d-mobile-block">Phone: +91-7016808730</span>
+                </p>
+                <p className="text-center">Address: C5, 304 Shukan Smile City, New ranip, Ahedabad, Gujrat, India.</p>
+            </div>
+            <hr />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title fw-bold pb-3">Skills</h5>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div>
+                                            <p className="mb-0">HTML</p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: "80%" }}>80%</div>
+                                            </div>
+                                        </div>
+                                        <div className="pt-3">
+                                            <p className="mb-0">CSS</p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: "70%" }} >70%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div>
+                                            <p className="mb-0">Java Script</p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: "50%" }}>40%</div>
+                                            </div>
+                                        </div>
+                                        <div className="pt-3">
+                                            <p className="mb-0">Python</p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: "80%" }} >80%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title fw-bold pb-3">Hobbies</h5>
+                                <span class="badge bg-primary me-3 mb-3 p-2">Primary</span>
+                                <span class="badge bg-secondary me-3 mb-3 p-2">Secondary</span>
+                                <span class="badge bg-success me-3 mb-3 p-2">Success</span>
+                                <span class="badge bg-danger me-3 mb-3 p-2">Danger</span>
+                                <span class="badge bg-warning text-dark me-3 mb-3 p-2">Warning</span>
+                                <span class="badge bg-info text-dark me-3 mb-3 p-2">Info</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
