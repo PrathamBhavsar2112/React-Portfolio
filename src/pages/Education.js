ba420ee83de30cb6bd399827cd27fa1ae7ffb6d9
Harshil Bhavsar <harshilb150799@gmail.com>
2022-06-22 13:35:03 +0530

    add edu and exp

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -3,7 +3,33 @@ import React from "react";
 function Education() {
     return (
         <React.Fragment>
-            Education
+            <div className="min-h-screen">
+                <div className="container py-5">
+                    <h1 className="text-center mb-5">Education</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Bachelor of Engineering <span className="h6">from Silver Oak University</span></h5>
+                                    <p className="mb-0">September,2020 - PRESENT</p>
+                                    <small>I am a student of silver oak university currently persuing Bachelor's degree in Information Technology.  </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">12th(science) GHSEB <span className="h6">from Mangaldeep School</span></h5>
+                                    <p className="mb-0">July 2019 - MAR 2019</p>
+                                    <small>I study my highschool year in mangaldeep school. spend most of the time prepering for GHSEB and JEE exam. </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
         </React.Fragment>
     );
 }
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,7 +3,33 @@ import React from "react";
 function Experience() {
     return (
         <React.Fragment>
-            Experience
+            <div className="min-h-screen">
+                <div className="container py-5">
+                    <h1 className="text-center mb-5">Experience</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Internship<span className="h6">at Coding Gujju</span></h5>
+                                    <p className="mb-0">Jun 2022 - PRESENT</p>
+                                    <small>Learn about HTML,CSS and create a personal portfolio using figma app. Learn how to host your website in localhost using React.</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">FrontEnd Developer <span className="h6">at Microsoft</span></h5>
+                                    <p className="mb-0">APR 2018 - APR 2020</p>
+                                    <small>Work as Frontend Developer at Microsoft. learn about backend as well frontend.</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
